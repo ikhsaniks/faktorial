de03485129f11e58c3e586600fac085bf301a807
ikhsaniks <miksya@gmail.com>
2017-04-08 12:03:06 +0700

    Add files via upload

---
+++ Ketupat.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ketupat;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Ketupat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+        int l = 2;
+        int tingkat = 10;
+        int n = 4;
+       
+        for(int i=1;i<=n;i++){
+            for(int k=n;k>i;k--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=l;j++){
+                System.out.print("+");
+            }
+            l+=2;
+
+            System.out.println("");
+        }
+        
+        l -= 4;
+        for(int i=(n-1);i>=1;i--){
+            for(int k=i;k<=(n-1);k++){
+                System.out.print(" ");
+            }
+            for(int j=l;j>=1;j--){
+                System.out.print("+");
+            }
+            l-=2;
+
+            System.out.println("");
+        }
+    }
+}
